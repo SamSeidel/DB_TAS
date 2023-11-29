753804bd52ff36ec3ffb05c5c562e24ee5f6fb9a
Sam <113889318+SamSeidel@users.noreply.github.com>
2023-11-29 13:55:31 +0100

    Add files via upload

--- dbprojekt/formulare/Teilnehmerdaten.php
+++ dbprojekt/formulare/Teilnehmerdaten.php
@@ -1,40 +1,74 @@
-<form action="/teilnehmer/speichern" method="post">
-  <div class="form-group">
-    <label for="vorname">Vorname</label>
-    <input type="text" name="vorname" id="vorname" required>
-  </div>
-  <div class="form-group">
-    <label for="nachname">Nachname</label>
-    <input type="text" name="nachname" id="nachname" required>
-  </div>
-  <div class="form-group">
-    <label for="anrede">Anrede</label>
-    <select name="anrede" id="anrede" required>
-      <option value="herr">Herr</option>
-      <option value="frau">Frau</option>
-      <option value="divers">Divers</option>
-    </select>
-  </div>
+<!DOCTYPE html>
+<html lang="de">
+<head>
+  <meta charset="UTF-8">
+  <title>Kursformular</title>
+</head>
+<body>
 
-  <?php
-  if ($vorname !== "" && $nachname !== "") {
-    echo "<div class=\"form-group\">
-      <label for=\"kurs\">Kurs</label>
-      <select name=\"kurs\" id=\"kurs\" required>
-        <option value=\"kursteilnehmer1\" selected>KursTeilnehmer1 *</option>
-        <option value=\"kursteilnehmer2\">KursTeilnehmer2</option>
+  <h1>Kursformular</h1>
+
+  <form action="index.php" method="post">
+
+    <input type="hidden" name="action" value="submit">
+
+    <div class="form-group">
+      <label for="vorname">Vorname</label>
+      <input type="text" class="form-control" id="vorname" name="vorname">
+    </div>
+
+    <div class="form-group">
+      <label for="nachname">Nachname</label>
+      <input type="text" class="form-control" id="nachname" name="nachname">
+    </div>
+
+    <div class="form-group">
+      <label for="geschlecht">Geschlecht</label>
+      <select class="form-control" id="geschlecht" name="geschlecht">
+        <option value="männlich">männlich</option>
+        <option value="weiblich">weiblich</option>
+        <option value="divers">divers</option>
       </select>
     </div>
 
-    <div class=\"form-group\">
-      <label for=\"zeitraum\">Zeitraum</label>
-      <select name=\"zeitraum\" id=\"zeitraum\" required>
-        <option value=\"blockzeitrauma\">Blockzeitraum A</option>
-        <option value=\"blockzeitraumb\" selected>Blockzeitraum B</option>
-      </select>
-    </div>";
-  }
-  ?>
+    <div class="form-group">
+      <label for="kurs">Kurs</label>
+      <select class="form-control" id="kurs" name="kurs">
+        <?php
 
-  <button type="submit">Weiter</button>
-</form>
+        // Verbindung zur Datenbank herstellen
+        $servername = "localhost";
+        $username = "root";
+        $password = "";
+        $dbname = "tas";
+
+        try {
+          $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+          // set the PDO error mode to exception
+          $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        } catch (PDOException $e) {
+          echo "Verbindung zur Datenbank fehlgeschlagen: " . $e->getMessage();
+        }
+
+        // Kurse aus der Datenbank auslesen
+        $sql = "SELECT Kurstyp, Gebühr, KursBeginn FROM kurs";
+        $stmt = $conn->prepare($sql);
+        $stmt->execute();
+
+        // Kurse in der Auswahlliste darstellen
+        while ($row = $stmt->fetch()) {
+          echo "<option value=\"{$row['Kurstyp']}\">{$row['Kurstyp']} ({$row['Gebühr']} €)  ({$row['KursBeginn']})</option>";
+        } 
+
+        // Verbindung zur Datenbank schließen
+        $conn = null;
+        ?>
+      </select>
+    </div>
+
+    <button type="submit" class="btn btn-primary">Abschicken</button>
+
+  </form>
+
+</body>
+</html>
