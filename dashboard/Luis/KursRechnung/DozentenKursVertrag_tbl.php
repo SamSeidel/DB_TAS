e6104f7ab9b1a9c5e24a0b9859f19694ce135ead
LuisBreg <143407757+LuisBreg@users.noreply.github.com>
2023-12-06 08:58:47 +0100

    Add files via upload

--- dashboard/Luis/KursRechnung/DozentenKursVertrag_tbl.php
+++ dashboard/Luis/KursRechnung/DozentenKursVertrag_tbl.php
@@ -6,112 +6,82 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Tabelle</title>
     <link rel="stylesheet" href="../css/style.css">
-    
 </head>
 <body>
 
-<form action="vertrag_neu_dozent.php" method="POST">
-
-<input name="id_Rechnung2" type="text" class="edit" placeholder="ID_Rechnung:">
-<input name="Vertragsgegenstand2" type="text" class="edit" placeholder="Vertragsgegenstand:">
-<input name="Vertragsbeginn2" type="date" class="edit" placeholder="Vertragsbeginn:">
-<input name="Vertragsende2" type="date" class="edit" placeholder="Vertragsende:">
-<input name="Honorar2" type="text" class="edit" placeholder="Honorar:">
-<input name="ID_Dozent2" type="text" class="edit" placeholder="ID_Dozent:">
-
-<button type="submit" name="Butt_VER_NEU" style="border:none; background-color: #ececec;"> 
-
-</form>
-
-
-
 <?php
-
-
-
-
-
-
 include 'dbinclude.php';
 
-$conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
-$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-$stmt = $conn->prepare("SELECT dozent.ID_Dozent, dozent.Steuernummer, dozent.Kürzel, dozentenvertrag.ID_Honorarvertrag, dozentenvertrag.Vertragsgegenstand, dozentenvertrag.Vertragsbeginn, dozentenvertrag.Vertragsende, dozentenvertrag.Honorar, dozentenvertrag.ID_Dozent 
-FROM dozentenvertrag
-LEFT JOIN dozent ON dozent.ID_Dozent = dozentenvertrag.ID_Dozent");
+try {
+    $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-$stmt->execute();
+    $stmt = $conn->prepare("SELECT kurs_dozent.Datum_Beginn, kurs_dozent.Datum_Ende, kurs_dozent.Anzahl_Einheiten, dozent.ID_Dozent, dozent.Kürzel, kurs.ID_Kurs, kurs.Kurstyp FROM kurs_dozent
+                            LEFT JOIN dozent ON dozent.ID_Dozent = kurs_dozent.ID_Dozent
+                            LEFT JOIN kurs ON kurs.ID_Kurs = kurs_dozent.ID_Kurs");
 
-/*
-while ($row = $stmt->fetch()) { 
-print_r($row);
-}
-*/
+    $stmt->execute();
+
 ?>
 
+<div>
+    <br><br><br>
+    <table style="width: 100%;">
+        <th> Beginn </th>
+        <th> Ende </th>
+        <th> Anzahl Einheit </th>
+        <th> Dozenten </th>
+        <th> Kurs </th>
+        <th></th>
 
+        <?php while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) { ?>
+            <tr>
+                <td> <input value="<?php echo $row["Datum_Beginn"] ?>" name="Beginn" type="text" class="edit"></td>
+                <td> <input value="<?php echo $row["Datum_Ende"] ?>" name="Ende" type="text" class="edit"></td>
+                <td> <input value="<?php echo $row["Anzahl_Einheiten"] ?>" name="Einheiten" type="text" class="edit"></td>
+                <td>
+                <input list="dozenten" name="dozent">
+  <datalist id="dozenten">
+                        <option select disabled value="-1">Wähle Deinen Dozenten</option>
+                        <option value="<?php echo $row['ID_Dozent'] ?>"><?php echo $row["Kürzel"] ?></option>
+                    </select>
+                </td>
 
-<div><br><br><br>
-            <table style="width: 100%;"> 
-                    <th> ID </th>
-                    <th> Kürzel </th>
-                    <th> Steuernummer </th>
-                    <th> Vertragsgegenstand </th>
-                    <th> Vertragsbeginn </th>
-                    <th> Vertragsende </th>
-                    <th> Honorar </th>
-                    <th> ID_Dozent </th>
-                    <th></th>
+                <td>
+                <input list="kurse" name="kurs">
+  <datalist id="kurse">
+                        <option select disabled value="-1">Wähle Deinen Kurs</option>
+                        <option value="<?php echo $row['ID_Kurs'] ?>"><?php echo $row["Kurstyp"] ?></option>
+                    </select>
+                </td>
+                
+                <td>
+                    <form action="Kurs_DOZ_Rechnung_AND.php" method="POST">
+                        <button type="submit" name="ID_rechnung" value="<?php echo $row["ID_Dozent"] ?>" style="border:none; background-color: #ececec;">
+                            <img src="../../res/recycling.png" style="width:24px; height:24px;">
+                        </button>
+                    </form>
+                </td>
 
-                <?php 
-                    while($row = $stmt->fetch()) {
-                        ?>
+                <td>
+                    <form action="DozenternKursvertrag_Lösch.php" method="POST">
+                        <button type="submit" name="ID_rechnung" value="<?php echo $row["ID_Kurs"] ?>" style="border:none; background-color: #ececec;">
+                            <img src="../../res/entfernen.png" style="width:24px; height:24px; background-color: #ececec;">
+                        </button>
+                    </form>
+                </td>
+            </tr>
+        <?php } ?>
+    </table>
+</div>
 
-                        <tr> 
-                        <form action="Rechnung_AND.php" method="POST">
-                            <td> <input value="<?php echo $row["ID_Honorarvertrag"]?>" name="id" type="text" class="edit"></td>
-                            <td> <input value="<?php echo $row["Kürzel"]?>" name="kuerzel" type="text" class="edit"></td>
-                            <td> <input value="<?php echo $row["Steuernummer"]?>" name="steuernum" type="text" class="edit"></td>
-                            <td> <input value="<?php echo $row["Vertragsgegenstand"]?>" name="vergegenst" type="text" class="edit"></td>
-                            <td> <input value="<?php echo $row["Vertragsbeginn"]?>" name="verbeginn" type="text" class="edit"></td>
-                            <td> <input value="<?php echo $row["Vertragsende"]?>" name="verende" type="text" class="edit"></td>
-                            <td> <input value="<?php echo $row["Honorar"]?>" name="honorar" type="text" class="edit"></td>
-                            <td> <input value="<?php echo $row["ID_Dozent"]?>" name="id_dozent" type="text" class="edit"></td>
-                            
-                            <td>  
-                                    <button type="submit" name="ID_vertrag" value="<?php echo $row["ID_Honorarvertrag"]?>" style="border:none; background-color: #ececec;"> 
-                                            <img src="../../res/recycling.png" style="width:24px; height:24px;">
-                                    </button>
-                                    </form>
-                            </td>
-    
-                            <td>
+<?php
+} catch (PDOException $e) {
+    echo "Fehler: " . $e->getMessage();
+}
 
-                                <form action="Dozenternvertrag_Lösch.php" method="POST">
-                                    <button type="submit" name="ID_vertrag" value="<?php echo $row["ID_Honorarvertrag"]?>" style="border:none; background-color: #ececec;">
-                                            <img src="../../res/entfernen.png" style="width:24px; height:24px; background-color: #ececec;">
-                                    </button>
-                                </form>
-                            </td>
-                        </tr>
-                        <?php
-                    }
-                ?>
-            <!-- <tr> 
-                <td style=" border-top: 1px solid black; border-collapse: collapse;"> </td>
-                <td style=" border-top: 1px solid black; border-collapse: collapse;"> </td>
-                <td style=" border-top: 1px solid black; border-collapse: collapse;"> </td>
-                <td style=" border-top: 1px solid black; border-collapse: collapse;"> </td>
-                <td style=" border-top: 1px solid black; border-collapse: collapse;"> </td>
-                <td style=" border-top: 1px solid black; border-collapse: collapse;"> </td>
-                <td style=" border-top: 1px solid black; border-collapse: collapse;"> </td>
-                <td style=" border-top: 1px solid black; border-collapse: collapse;"> </td>
-                <td style=" border-top: 1px solid black; border-collapse: collapse;"> </td>
-                <td style=" border-top: 1px solid black; border-collapse: collapse;"> </td>
-            </tr>-->
+$conn = null;
+?>
 
-        </table>
-    </div>
-        
 </body>
 </html>
