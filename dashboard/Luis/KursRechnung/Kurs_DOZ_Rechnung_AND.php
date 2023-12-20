69d943d160081f8907b51ae028db953f18358b75
LuisBreg <143407757+LuisBreg@users.noreply.github.com>
2023-12-20 09:01:58 +0100

    Add files via upload

--- dashboard/Luis/KursRechnung/Kurs_DOZ_Rechnung_AND.php
+++ dashboard/Luis/KursRechnung/Kurs_DOZ_Rechnung_AND.php
@@ -1,36 +1,24 @@
 <?php 
     include 'dbinclude.php';
+    print_r($_POST);
 
     $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-  
- 
-        //Mit isempty arbeiten
-       
-        if(!empty($_POST["Beginn"])) {
-            $conn->prepare("UPDATE kurs_dozent SET Datum_Beginn=':Datum_Beginn' WHERE ID=':ID' ")->execute();
-        }
-        if(!empty($_POST["Ende"])) {
-            $conn->prepare("UPDATE kurs_dozent SET Datum_Ende=':Datum_Ende' WHERE ID=':ID'  ")->execute();
-        }
-        if(!empty($_POST["Anzahl_Einheiten"])) {
-            $conn->prepare("UPDATE kurs_dozent SET Anzahl_Einheiten=':Anzahl_Einheiten' WHERE ID=':ID' ")->execute();
-        }
-        if(!empty($_POST["Dozent"])) {
-            $conn->prepare("UPDATE kurs_dozent SET 	ID_Dozent=':ID_Dozent' WHERE ID=':ID'")->execute();
-        }
-        if(!empty($_POST["kurs"])) {
-            $conn->prepare("UPDATE kurs_dozent SET ID_Kurs=':ID_Kurs' WHERE ID=':ID'")->execute();
-        }
-        
+        //datum Umformatieren
 
-        $conn->bindParam(':Datum_Beginn', $_POST["Beginn"]);
-$conn->bindParam(':Datum_Ende', $_POST["Ende"]);
-$conn->bindParam(':Anzahl_Einheiten', $_POST["Einheiten"]);
-$conn->bindParam(':ID_Dozent', $_POST["Dozent"]);
-$conn->bindParam(':ID_Kurs', $_POST["kurs"]);
-$conn->bindParam(':ID', $_POST["ID_rechnung"]);
-        
-    header('Location: '. "DozentenKursVertrag_tbl.php");
+      
+            $test=$conn->prepare("UPDATE kurs_dozent SET Datum_Beginn=:Datum_Beginn,Datum_Ende=:Datum_Ende, Anzahl_Einheiten=:Anzahl_Einheiten, ID_Dozent=:ID_Dozent, ID_Kurs=:ID_Kurs WHERE ID=:ID");
+
+            $test->bindParam(':Datum_Beginn', $_POST["Beginn"]);
+            $test->bindParam(':Datum_Ende', $_POST["Ende"]);
+            $test->bindParam(':Anzahl_Einheiten', $_POST["Einheiten"]);
+            $test->bindParam(':ID_Dozent', $_POST["Dozent"]);
+            $test->bindParam(':ID_Kurs', $_POST["kurs"]);
+            $test->bindParam(':ID', $_POST["ID_rechnung"]);
+
+
+$test->execute();        
+header('Location: '. "DozentenKursVertrag_tbl.php");
+
 ?>
