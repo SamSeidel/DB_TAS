b91b50255ad696a06a19be90f521872a66cc3156
LuisBreg <143407757+LuisBreg@users.noreply.github.com>
2024-01-10 00:35:23 +0100

    Add files via upload

---
+++ Luis/Rechnungen_Kurs_Vertrag/Kurs_doz_NEU.php
@@ -0,0 +1,23 @@
+<?php 
+    include 'dbinclude.php';
+
+
+  $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+  // set the PDO error mode to exception
+  $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+  $stmt = $conn->prepare("INSERT INTO kurs_dozent (Datum_Beginn, Datum_Ende, Anzahl_Einheiten, ID_Dozent, ID_Kurs)
+  VALUES (':Datum_Beginn', ':Datum_Ende', ':Anzahl_Einheiten', ':ID_Dozent', ':ID_Kurs' )");
+
+$stmt->bindParam(':Datum_Beginn', $_POST["Beginn2"]);
+$stmt->bindParam(':Datum_Ende', $_POST["Ende2"]);
+$stmt->bindParam(':Anzahl_Einheiten', $_POST["Einheiten2"]);
+$stmt->bindParam(':ID_Dozent', $_POST["Dozent2"]);
+$stmt->bindParam(':ID_Kurs', $_POST["kurs2"]);
+
+
+$stmt->execute();
+
+$conn = null;
+
+    header('Location: '. "DozentenKursVertrag_tbl.php");
+?>
