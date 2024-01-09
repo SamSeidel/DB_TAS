f3b8a63e4fba42cea3cfaedc0046fd78c73a7988
LuisBreg <143407757+LuisBreg@users.noreply.github.com>
2024-01-10 00:40:42 +0100

    Update Rechnung_AND.php

--- Luis/HonorarVertrag/Rechnung_AND.php
+++ Luis/HonorarVertrag/Rechnung_AND.php
@@ -1,29 +1,24 @@
 <?php 
     include 'dbinclude.php';
+    print_r($_POST);
 
     $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-    $id_user = $_POST["ID_vertrag"];
-        //Mit isempty arbeiten
+        //datum Umformatieren
 
-       
-        if(!empty($_POST["vergegenst"])) {
-            $conn->prepare("UPDATE dozentenvertrag SET Vertragsgegenstand='$_POST[vergegenst]'WHERE ID_Honorarvertrag=$id_user")->execute();
-        }
-        if(!empty($_POST["verbeginn"])) {
-            $conn->prepare("UPDATE dozentenvertrag SET Vertragsbeginn='$_POST[verbeginn]'WHERE ID_Honorarvertrag=$id_user")->execute();
-        }
-        if(!empty($_POST["verende"])) {
-            $conn->prepare("UPDATE dozentenvertrag SET Vertragsende='$_POST[verende]'WHERE ID_Honorarvertrag=$id_user")->execute();
-        }
-        if(!empty($_POST["honorar"])) {
-            $conn->prepare("UPDATE dozentenvertrag SET Honorar='$_POST[honorar]'WHERE ID_Honorarvertrag=$id_user")->execute();
-        }
-        if(!empty($_POST["ID_Dozent"])) {
-            $conn->prepare("UPDATE dozentenvertrag SET ID_Dozent='$_POST[id_dozent]'WHERE ID_Honorarvertrag=$id_user")->execute();
-        }
-        
-        
+      
+            $test=$conn->prepare("UPDATE dozentenvertrag SET Vertragsgegenstand=:Vertragsgegenstand,Vertragsbeginn=:Vertragsbeginn, Vertragsende=:Vertragsende, Honorar=:Honorar, ID_Dozent=:ID_Dozent WHERE ID_Honorarvertrag=:ID_Honorarvertrag");
+
+            $test->bindParam(':Vertragsgegenstand', $_POST["vergegenst"]);
+            $test->bindParam(':Vertragsbeginn', $_POST["verbeginn"]);
+            $test->bindParam(':Vertragsende', $_POST["verende"]);
+            $test->bindParam(':Honorar', $_POST["honorar"]);
+            $test->bindParam(':ID_Dozent', $_POST["ID_Dozent"]);
+            $test->bindParam(':ID_Honorarvertrag', $_POST["ID_vertrag"]);
+
+
+$test->execute();        
     header('Location: '. "DozentenVertrag_tbl.php");
+
 ?>
